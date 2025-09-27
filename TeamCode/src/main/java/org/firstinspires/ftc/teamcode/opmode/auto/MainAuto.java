9232a2c6d0d334d13b54db734278a6073761044b
lukewhite32 <lukebenwhite32@gmail.com>
2025-09-27 19:00:13 -0400

    Basic project structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/MainAuto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class MainAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleop.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class MainTeleop extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+}
