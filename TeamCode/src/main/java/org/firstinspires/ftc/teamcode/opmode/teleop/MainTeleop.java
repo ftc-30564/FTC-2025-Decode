ba7d5eee445be326b8e63751eed7775a0ab9cad3
lukewhite32 <lukebenwhite32@gmail.com>
2025-10-02 16:38:47 -0400

    Added motor stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleop.java
@@ -2,12 +2,15 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class MainTeleop extends LinearOpMode {
 
     @Override
     public void runOpMode() {
         DcMotor motor = hardwareMap.get(DcMotor.class, "motor");
+
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
