81bdc09b93711f5efa1cd3cdda8f6079bd52b02b
lukewhite32 <lukebenwhite32@gmail.com>
2025-10-02 18:22:42 -0400

    basic auto configuration, tested and works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleop.java
@@ -2,20 +2,17 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class MainTeleop extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        DcMotor motor = hardwareMap.get(DcMotor.class, "motor");
-
-        motor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        waitForStart();
 
         while (opModeIsActive()) {
-            motor.setPower(0.5);
+
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -27,16 +27,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import java.util.ArrayList;
 import java.util.List;
 
-// For Panels
-import com.bylazar.configurables.annotations.Configurable;
-import com.bylazar.configurables.annotations.IgnoreConfigurable;
-import com.bylazar.configurables.PanelsConfigurables;
-import com.bylazar.field.FieldManager;
-import com.bylazar.field.PanelsField;
-import com.bylazar.field.Style;
-import com.bylazar.telemetry.PanelsTelemetry;
-import com.bylazar.telemetry.TelemetryManager;
-
 /**
  * This is the Tuning class. It contains a selection menu for various tuning OpModes.
  *
