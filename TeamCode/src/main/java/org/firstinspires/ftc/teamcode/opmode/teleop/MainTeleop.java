6a90aa6b89c61aa77f1423f6b93a0b4a155c3932
lukewhite32 <lukebenwhite32@gmail.com>
2025-10-02 16:35:42 -0400

    Added motor code to MainTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/MainTeleop.java
@@ -1,16 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class MainTeleop extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        DcMotor motor = hardwareMap.get(DcMotor.class, "motor");
 
         waitForStart();
 
         while (opModeIsActive()) {
-
+            motor.setPower(0.5);
         }
     }
 }
