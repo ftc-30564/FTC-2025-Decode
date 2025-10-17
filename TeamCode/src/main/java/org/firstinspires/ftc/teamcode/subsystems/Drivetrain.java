c6787997ba5205987a2155198ec6e42fab82f494
Shubb01 <229819682+swhubb01-svg@users.noreply.github.com>
2025-10-16 17:20:06 -0700

    added follower to drivetrain and replaced it in MainTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,10 +1,32 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
 public class Drivetrain {
+    private Follower follower;
 
     public Drivetrain(HardwareMap hardwareMap) {
+        follower = Constants.createFollower(hardwareMap);
 
+    }
+
+    public void startTeleopDrive(){
+        follower.startTeleopDrive();
+    }
+    public void setTeleopDrive(double forward, double strafe, double turn, boolean isRobotCentric){
+        follower.setTeleOpDrive(forward, strafe, turn, isRobotCentric);
+    }
+    public void update(){
+        follower.update();
+    }
+    public Pose getPose(){
+        return follower.getPose();
+    }
+    public void setStartingPose(Pose pose){
+        follower.setStartingPose(pose);
     }
 }
