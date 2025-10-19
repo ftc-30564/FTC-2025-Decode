9e23d0874f78e37ae27a430e53819ece4ce5a8a3
lukewhite32 <lukebenwhite32@gmail.com>
2025-10-19 16:11:15 -0400

    interpolator code written. needs testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Interpolator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Interpolator.java
@@ -11,9 +11,22 @@ public class Interpolator {
     public Interpolator(ArrayList<Point> points) {
         this.points = points;
     }
+    public int getNearestPointIndex(double distance) {
+        for (int x = 0; x < points.size(); x ++) {
+            if (points.get(x).x > distance) {
+               return x - 1;
+            }
+        }
+        throw new IndexOutOfBoundsException("Could not find nearest point in interpolator");
+    }
 
     // Returns the ideal wheel velocity based off of the distance from the goal
     public double getVelocity(double distance) {
-        return 0;
+        Point leftPoint = points.get(getNearestPointIndex(distance));
+        Point rightPoint = points.get(getNearestPointIndex(distance) + 1);
+
+        double slope = (rightPoint.y - leftPoint.y) / (rightPoint.x - leftPoint.x);
+        double yIntercept = (leftPoint.y - (slope * leftPoint.x));
+        return slope * distance + yIntercept;
     }
 }
