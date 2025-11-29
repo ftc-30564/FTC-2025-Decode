79a29a1b75fa97a4235bf820a37567b7df02a64f
lukewhite32 <lukebenwhite32@gmail.com>
2025-11-29 15:47:52 -0500

    add morse code reader that reads from assets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/debug/MorseCodeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/debug/MorseCodeTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.subsystems.IndicatorRGB;
 import org.firstinspires.ftc.teamcode.subsystems.MorseCodePlayer;
+import org.firstinspires.ftc.teamcode.util.MorseCodeReader;
 
 @Autonomous
 public class MorseCodeTest extends LinearOpMode {
@@ -12,9 +13,10 @@ public class MorseCodeTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        MorseCodeReader reader = new MorseCodeReader(hardwareMap);
         MorseCodePlayer player = new MorseCodePlayer(new IndicatorRGB(hardwareMap));
+        player.addSequence(reader.getMorseCode());
 
-        player.addSequence(".-.. .. -.- ./.- -. -../... ..- -... ... -.-. .-. .. -... .");
         waitForStart();
 
         while (opModeIsActive()) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MorseCodeReader.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import android.content.res.AssetManager;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.subsystems.MorseCodePlayer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+// reads the morse code message from assets
+public class MorseCodeReader {
+    public AssetManager manager;
+
+    public MorseCodeReader(HardwareMap hardwareMap) {
+        this.manager = hardwareMap.appContext.getApplicationContext().getAssets();
+    }
+
+    public String getMorseCode() {
+        try {
+            InputStream inputStream = this.manager.open("morse-code.txt");
+
+            Scanner s = new Scanner(inputStream).useDelimiter("\\A");
+            String result = s.hasNext() ? s.next() : "-";
+
+            return result;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // lol
+        return "...---...";
+    }
+}
