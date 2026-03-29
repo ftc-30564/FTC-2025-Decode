15190654acd772f5e51cb56297a37a8e76590db7
Shubb01 <229819682+swhubb01-svg@users.noreply.github.com>
2026-03-29 15:56:57 -0700

    change intake to motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,14 +1,14 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.RobotConstants;
 
 public class Intake {
-    private CRServo firstIntake;
-    private CRServo secondIntake;
+    private DcMotor firstIntake;
 
     public static enum Mode {
         RUNNING,
@@ -17,26 +17,21 @@ public class Intake {
     }
 
     public Intake(HardwareMap hardwareMap) {
-      firstIntake = hardwareMap.get(CRServo.class, RobotConstants.Intake.FIRST_INTAKE_NAME);
-      secondIntake = hardwareMap.get(CRServo.class, RobotConstants.Intake.SECOND_INTAKE_NAME);
+      firstIntake = hardwareMap.get(DcMotor.class, RobotConstants.Intake.FIRST_INTAKE_NAME);
 
       firstIntake.setDirection(DcMotorSimple.Direction.REVERSE);
     }
     public void run() {
         firstIntake.setPower(RobotConstants.Intake.FIRST_INTAKE_RUN_SPEED);
-        secondIntake.setPower(RobotConstants.Intake.SECOND_INTAKE_RUN_SPEED);
     }
 
     public void stop() {
         firstIntake.setPower(0);
-        secondIntake.setPower(0);
     }
     public void barf() {
         firstIntake.setPower(-RobotConstants.Intake.FIRST_INTAKE_RUN_SPEED);
-        secondIntake.setPower(-RobotConstants.Intake.SECOND_INTAKE_RUN_SPEED);
     }
     public void spit() {
         firstIntake.setPower(1);
-        secondIntake.setPower(-1);
     }
 }
